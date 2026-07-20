68d3003bdf1a2fb45479aa12d82a8550fd29f583
Gara <gara@caramel.city>
2026-07-21 04:00:58 +0800

    ~

--- ESPIDF-Proj/Projects/01_led/main/main.c
+++ ESPIDF-Proj/Projects/01_led/main/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
 
 void app_main(void)
 {
-    printf("Hello World!!\n");
+    led_init();
+    vTaskDelay(500);
+
+    while(1)
+    {
+        LED_TOGGLE();
+        vTaskDelay(500);
+    }
 }
